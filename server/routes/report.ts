14d80473ea207f9f74780ad3efa3c87fefb31ec6
unknown <molnarandre@outlook.com>
2021-12-24 17:23:22 +0100

    filter reports by date

--- server/routes/report.ts
+++ server/routes/report.ts
@@ -2,8 +2,12 @@ import Report from "../models/report";
 import express from "express";
 const router = express.Router();
 
-router.get("/reports", async (req, res) => {
-  res.send(await Report.find());
+router.get("/reports/:date", async (req, res) => {
+  res.send(
+    await Report.find({
+      lastUpdatedAtApify: req.params.date,
+    })
+  );
 });
 
 export default router;
